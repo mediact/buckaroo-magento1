30c7524d4a19e8661fa796508e34b22beb191412
Mickey Beijer <mickey.beijer@tig.nl>
2019-04-15 14:32:04 +0200

    BUCKM1-569 : Removed ConversationLanguage

--- app/code/community/TIG/Buckaroo3Extended/Model/PaymentMethods/Afterpay20/Observer.php
+++ app/code/community/TIG/Buckaroo3Extended/Model/PaymentMethods/Afterpay20/Observer.php
@@ -273,7 +273,6 @@ class TIG_Buckaroo3Extended_Model_PaymentMethods_Afterpay20_Observer
         $addressInfo[] = $this->getParameterLine('Country', $address->getCountryId(), $addressType);
         $addressInfo[] = $this->getParameterLine($phonenumberKey, $phonenumber['clean'], $addressType);
         $addressInfo[] = $this->getParameterLine('Email', $address->getEmail(), $addressType);
-        $addressInfo[] = $this->getParameterLine('ConversationLanguage', $address->getCountryId(), $addressType);
 
 
         //TODO: Enable when finnish id number is implemented
