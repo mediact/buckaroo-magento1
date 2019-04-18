49de6997e9bed5fe3acba556c04b0c1c71133ea0
Kevin van Dijk <kevin.van.dijk@tig.nl>
2019-04-18 15:12:10 +0200

    fixed year select dropdown

--- app/code/community/TIG/Buckaroo3Extended/Model/Sources/CreditcardIssuers.php
+++ app/code/community/TIG/Buckaroo3Extended/Model/Sources/CreditcardIssuers.php
@@ -9,14 +9,13 @@ class TIG_Buckaroo3Extended_Model_Sources_CreditcardIssuers
             array('value' => 'American Express', 'label' => $helper->__('American Express'), 'code' => 'amex', 'logo' => '/creditcard_types/creditcard_americanexpress.png'),
             array('value' => 'Mastercard', 'label' => $helper->__('Mastercard'), 'code' => 'mastercard', 'logo' => "/creditcard_types/creditcard_mastercard.png"),
             array('value' => 'Visa', 'label' => $helper->__('Visa'), 'code' => 'visa', 'logo' => '/creditcard_types/creditcard_visa.png'),
-            array('value' => 'Visa Electron', 'label' => $helper->__('Visa Electron'), 'code' => 'visa', 'logo' => '/creditcard_types/creditcard_visa.png'),
+            array('value' => 'Visa Electron', 'label' => $helper->__('Visa Electron'), 'code' => 'visaelectron', 'logo' => '/creditcard_types/creditcard_visa.png'),
             array('value' => 'Maestro', 'label' => $helper->__('Maestro'), 'code' => 'maestro', 'logo' => '/creditcard_types/creditcard_maestro.png'),
-            array('value' => 'Dankort', 'label' => $helper->__('Dankort'), 'code' => 'amex', 'logo' => '/creditcard_types/creditcard_americanexpress.png'),
-            array('value' => 'Carte Bancaire', 'label' => $helper->__('Carte Bancaire'), 'code' => 'visa', 'logo'=> "/creditcard_types/creditcard_visa.png"),
-            array('value' => 'Carte Bleue', 'label' => $helper->__('Carte Bleue'), 'code' => 'visa', 'logo' => '/creditcard_types/creditcard_visa.png'),
-            array('value' => 'VPay', 'label' => $helper->__('VPay'), 'code' => 'visa', 'logo' => '/creditcard_types/creditcard_visa.png'),
-            array('value' => 'Nexi', 'label' => $helper->__('Nexi'), 'code' => 'amex', 'logo' => '/creditcard_types/creditcard_americanexpress.png'),
-            array('value' => 'Switch', 'label' => $helper->__('Switch'), 'code' => 'maestro', 'logo' => '/creditcard_types/maestro.png'),
+            array('value' => 'Dankort', 'label' => $helper->__('Dankort'), 'code' => 'dankort', 'logo' => '/creditcard_types/creditcard_dankort.png'),
+            array('value' => 'Carte Bancaire', 'label' => $helper->__('Carte Bancaire'), 'code' => 'cartebancaire', 'logo'=> "/creditcard_types/creditcard_cartebancaire.png"),
+            array('value' => 'Carte Bleue', 'label' => $helper->__('Carte Bleue'), 'code' => 'cartebleuevisa', 'logo' => '/creditcard_types/cartebleuevisa.png'),
+            array('value' => 'VPay', 'label' => $helper->__('VPay'), 'code' => 'vpay', 'logo' => '/creditcard_types/creditcard_vpay.png'),
+            array('value' => 'Nexi', 'label' => $helper->__('Nexi'), 'code' => 'nexi', 'logo' => '/creditcard_types/creditcard_nexi.png'),
         );
     }
 }
